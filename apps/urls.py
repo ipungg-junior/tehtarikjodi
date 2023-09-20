5e6e61aaa3befcce6748b7b77002e3b2d0fd974d
ipungg-junior <ipungg.id@gmail.com>
2023-09-20 15:26:37 +0700

    ROI Feature Functional

--- apps/urls.py
+++ apps/urls.py
@@ -4,5 +4,6 @@ from apps.views import LandingPage, Simulation
 urlpatterns = [
     path('', LandingPage.as_view(context="")),
     path('simulasi-investasi/', Simulation.as_view(context="")),
-    path('simulasi-investasi/post-calculate/', Simulation.as_view(context="post-calculate"))
+    path('simulasi-investasi/post-calculate/', Simulation.as_view(context="post-calculate")),
+    path('simulasi-investasi/report/', Simulation.as_view(context="report-simulation"))
 ]
